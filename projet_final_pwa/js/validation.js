4256f97581adbfe2a8c5e7f3378ade2e0bc28157
prodevweb21 <prodevweb21@gmail.com>
2021-09-16 14:24:02 -0400

    Projet PWA

---
+++ projet_final_pwa/js/validation.js
@@ -0,0 +1,31 @@
+// Dedié à la partie header de la page : patrimoine
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
+
+
+// Dédié à la page guide_voyage formulaire
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
